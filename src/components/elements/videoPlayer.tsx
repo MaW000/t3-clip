210ab99477c591b84a5edd7a240478c2f87ab286
Matthew Wardlow <mwardlow0@gmail.com>
2023-03-19 04:20:52 -0400

    ty

--- src/components/elements/videoPlayer.tsx
+++ src/components/elements/videoPlayer.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useEffect } from "react";
 import type { RefObject } from "react";
 import { VodParse, TwitchEmbed, EmoteCarousel } from "@/elements";
 import { api } from "~/utils/api";
@@ -17,15 +17,32 @@ export const VideoDash = ({ videoId }: { videoId: number }) => {
   const videoSaveRes = api.video.getVideo.useQuery({ videoId: videoId });
   const response = videoSaveRes.data;
   const playerRefFunc = useRef<(() => void) | null>(null);
+  useEffect(() => {
+    if (!playerRef.current?.clientWidth) return;
 
+    const w = playerRef.current.clientWidth - 50;
+    const h = (9 / 16) * w;
+    const options = {
+      width: w,
+      height: h + 0.11,
+      video: videoId,
+      time: "0h0m1s",
+    };
+
+    if (playerRefFunc?.current === null) {
+      const initPlayer = new (window as any).Twitch.Player("player", options);
+      playerRefFunc.current = initPlayer;
+    }
+  }, [videoId]);
   return (
     <div className="relative">
       <div className="mx-5 bg-slate-700 md:grid md:grid-cols-12">
-        <TwitchEmbed
-          playerRef={playerRef}
-          playerRefFunc={playerRefFunc}
-          videoId={videoId}
-        />
+        <div
+          id="player"
+          ref={playerRef}
+          className=" col-span-9 col-start-1  row-span-full  "
+        ></div>
+
         <VodParse
           playerRefFunc={playerRefFunc}
           playerRef={playerRef}
